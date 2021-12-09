79d150ae91a4c2be98e7fdad400e28f351a486f6
MylesACD <49326725+MylesACD@users.noreply.github.com>
2021-12-08 17:26:55 -0800

    added gitnore for pyc

--- analysis.py
+++ analysis.py
@@ -10,7 +10,11 @@ days = years *365
 # duration is a length of years split into seconds
 dur= days *24 *60*60
 #measured in seconds one hour
-dt = 60 *60
+hour = 3600
+
+day = 24*hour
+
+year = 365*hour
 
 frame_time = 0.001
 #---------------------
@@ -18,7 +22,7 @@ frame_time = 0.001
 
 def distance_e_s():
     bodies = [Sim.SUN,Sim.EARTH]
-    list_of_positions, b_list  = Sim.orbit_sim(bodies,dur,dt)
+    list_of_positions, b_list  = Sim.orbit_sim(bodies,dur,hour)
     
     distances = []
     
@@ -30,12 +34,19 @@ def distance_e_s():
     plt.ylabel("distance (m)")
     plt.title("Distance Between Earth and Sun Over 100 Years")
     
-    list_of_positions, b_list  = Sim.orbit_sim(bodies,dur,dt*24)
+    list_of_positions, b_list  = Sim.orbit_sim(bodies,dur,day)
     distances = []
     for bodies in b_list:
         distances.append(b.calc_square_distance(bodies[0],bodies[1])**0.5)
     
     plt.plot(range(len(distances)),distances, "red")
+    
+    list_of_positions, b_list  = Sim.orbit_sim(bodies,dur,year)
+    distances = []
+    for bodies in b_list:
+        distances.append(b.calc_square_distance(bodies[0],bodies[1])**0.5)
+    
+    plt.plot(range(len(distances)),distances, "black")
     
 
 distance_e_s()
